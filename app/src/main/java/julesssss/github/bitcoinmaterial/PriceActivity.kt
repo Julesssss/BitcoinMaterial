4a25d715a2d5aeab869c6432f1df8202a058568e
Jules <julianrosser91@gmail.com>
2018-01-07 23:51:01 +0000

    create PriceActivity and basic layout

---
+++ app/src/main/java/julesssss/github/bitcoinmaterial/PriceActivity.kt
@@ -0,0 +1,14 @@
+package julesssss.github.bitcoinmaterial
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class PriceActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_price)
+    }
+
+}

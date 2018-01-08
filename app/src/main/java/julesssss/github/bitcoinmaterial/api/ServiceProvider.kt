60b72f4a28466a7b112325a601dfbd1d5432ee26
Jules <julianrosser91@gmail.com>
2018-01-08 00:29:41 +0000

    refactor service setup function to initCoinbaseService()

--- app/src/main/java/julesssss/github/bitcoinmaterial/api/ServiceProvider.kt
+++ app/src/main/java/julesssss/github/bitcoinmaterial/api/ServiceProvider.kt
@@ -10,17 +10,12 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
-/**
- * Provide required Service objects
- * - services: main endpoint used for user & device requests
- * - metadata: for retrieving subscription products
- */
 internal object ServiceProvider {
 
     private val gson: Gson
     private var okHttpClient: OkHttpClient
 
-    val coinbaseService by lazy { initCsgServiceApi() }
+    val coinbaseService by lazy { initCoinbaseService() }
 
     init {
         val interceptor = HttpLoggingInterceptor()
@@ -34,7 +29,7 @@ internal object ServiceProvider {
                 .build()
     }
 
-    private fun initCsgServiceApi(): CoinbaseService = Retrofit.Builder()
+    private fun initCoinbaseService(): CoinbaseService = Retrofit.Builder()
             .baseUrl(BuildConfig.COINBASE_ENDPOINT_V2)
             .addConverterFactory(GsonConverterFactory.create(gson))
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())

1046c0be9f4651b79e7ca30de4c93379aad994c0
Jules <julianrosser91@gmail.com>
2018-01-09 00:30:39 +0000

    add chart gradient

--- app/src/main/java/julesssss/github/bitcoinmaterial/ui/Price/PriceViewModel.kt
+++ app/src/main/java/julesssss/github/bitcoinmaterial/ui/Price/PriceViewModel.kt
@@ -10,6 +10,7 @@ import com.db.chart.view.LineChartView
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
+import julesssss.github.bitcoinmaterial.R
 import julesssss.github.bitcoinmaterial.api.ServiceProvider.coinbaseService
 import julesssss.github.bitcoinmaterial.data.PriceResponse
 import uk.co.ostmodern.util.SchedulerProvider
@@ -18,7 +19,7 @@ class PriceViewModel {
 
     private val mLabels = arrayOf("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "")
 
-    private val mValues = floatArrayOf(25f, 27f, 37f, 39f, 23f, 36f, 50f, 83f, 65f, 68f, 28f, 55f, 58f, 50f, 53f, 53f, 57f, 48f, 50f, 53f, 54f, 25f, 37f, 35f, 37f, 35f, 50f, 62f, 55f, 59f, 70f, 82f, 60f, 55f, 63f, 65f, 69f, 70f, 73f, 80f)
+    private val mValues = floatArrayOf(25f, 27f, 37f, 32f, 34f, 36f, 50f, 66f, 65f, 68f, 38f, 55f, 58f, 50f, 53f, 53f, 57f, 48f, 50f, 53f, 54f, 35f, 37f, 35f, 37f, 35f, 50f, 62f, 55f, 59f, 70f, 82f, 60f, 55f, 63f, 65f, 69f, 70f, 73f, 80f)
 
     private val scheduleProvider by lazy { SchedulerProvider(Schedulers.io(), AndroidSchedulers.mainThread()) }
 
@@ -31,21 +32,23 @@ class PriceViewModel {
         dataset.setColor(Color.parseColor("#ffffff"))
                 .setThickness(Tools.fromDpToPx(3f))
                 .setSmooth(true)
+                .setGradientFill(intArrayOf(R.color.colorGraphDark, Color.TRANSPARENT), floatArrayOf(0.20f, 0.75f))
                 .beginAt(0)
                 .endAt(40)
 
         mChart.addData(dataset)
 
         val thresPaint = Paint()
-        thresPaint.color = Color.parseColor("#ffffff")
+        thresPaint.color = mChart.resources.getColor(R.color.whiteFaded)
         thresPaint.style = Paint.Style.STROKE
         thresPaint.isAntiAlias = true
-        thresPaint.strokeWidth = Tools.fromDpToPx(1f)
+        thresPaint.strokeWidth = Tools.fromDpToPx(1.6f)
 
         mChart.setBorderSpacing(Tools.fromDpToPx(0f).toInt())
                 .setXLabels(AxisRenderer.LabelPosition.NONE)
                 .setLabelsColor(Color.parseColor("#ffffff"))
                 .setYLabels(AxisRenderer.LabelPosition.NONE)
+                .setValueThreshold(43f, 43f, thresPaint)
                 .setXAxis(false)
                 .setYAxis(false)
                 .setAxisBorderValues(0f, 110f)
